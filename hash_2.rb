4da0d57a6eea4ee349651fc31a03e77b9d1c4dd6
EC2 Default User <ec2-user@ip-10-0-0-115.ap-northeast-1.compute.internal>
2023-10-18 14:59:42 +0000

    add hashi

---
+++ hash_2.rb
@@ -0,0 +1,15 @@
+# 2次元配列の基本操作1
+team_a = ["勇者", "戦士", "魔法使い"]
+team_b = ["盗賊", "忍者", "商人"]
+team_c = ["スライム", "ドラゴン", "魔王"]
+
+teams = [team_a,team_b,team_c]
+p teams
+p teams[0]
+p teams[0][0]
+p teams[0][1]
+p teams[0][2]
+teams[0][1]="魔導士"
+p teams
+p teams.length
+p teams[0].length
